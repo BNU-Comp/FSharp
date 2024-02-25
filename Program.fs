bbdabfd7ac15860088ddfdc176dbd42a8044b7d3
Derek Peacock <derek.peacock@yahoo.com>
2024-02-25 19:20:49 +0000

    modified:   Program.fs

--- Program.fs
+++ Program.fs
@@ -4,8 +4,8 @@ open week01
 
 [<EntryPoint>]
 let main argv =
-    printfn "\nHello from F#"
-    printfn "Calling Week 01 Exercise 3"
+    printfn "\n  Hello from F#"
+    printfn "  Calling Week 01 Exercise 3"
     
 
     let result = week01.exercise3.run()
