b169b46bc1ec036344069c77fc5324576ab14b69
Derek Peacock <derek.peacock@yahoo.com>
2024-02-26 09:24:30 +0000

    Added exercise 7

--- week01.fs
+++ week01.fs
@@ -42,10 +42,21 @@ module exercise4 =
         let sum = num1 + num2
         printfn "The sum of %d and %d is %d\n" num1 num2 sum
 
-// A function that asks the user to enter two numbers and then choose an operator from +,-,*,/.  
-// The function then returns the result of addition, subtraction, multiplication or division
-// based on the user's choice.
+// A function that gets 2 integers from the user and returns the 
+// result of integer division of the first number by the second number
+// and the remainder.
 module exercise5 =
+    let run () =
+        let num1 = Helpers.getInteger "\nEnter first number > " 1 100
+        let num2 = Helpers.getInteger "Enter second number > " 1 100
+        let div = num1 / num2
+        let rem = num1 % num2
+        printfn "The result of %d / %d is %d with a remainder of %d\n" num1 num2 div rem
+
+// A function that asks the user to enter two numbers and then choose 
+// an operator from +,-,*,/.  The function then returns the result of 
+// addition, subtraction, multiplication or division based on the user's choice.
+module exercise6 =
     let run () =
         let num1 = Helpers.getInteger "\nEnter first number > " 1 100
         let num2 = Helpers.getInteger "Enter second number > " 1 100
@@ -59,4 +70,18 @@ module exercise5 =
             | "/" -> num1 / num2
             | _ -> 0
         printfn "The result of %d %s %d is %d\n" num1 op num2 result
+
+// A function that asks the user to enter a start number and 
+// an finish number as integers and then returns the sum of all
+// the numbers between the start and finish numbers.
+module exercise7 =
+    let run () =
+        let start = Helpers.getInteger "\nEnter start number > " 1 100
+        let finish = Helpers.getInteger "Enter finish number > " 1 100
+        let sum = 
+            let rec loop i acc =
+                if i > finish then acc
+                else loop (i + 1) (acc + i)
+            loop start 0
+        printfn "\n The sum of all numbers between %d and %d is %d\n" start finish sum
 
