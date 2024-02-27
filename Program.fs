c6a9c6dd24de781aa8238b0842e3b0b15d227773
Derek Peacock <derek.peacock@yahoo.com>
2024-02-27 14:54:27 +0000

    Added exercises 3 and 4

--- Program.fs
+++ Program.fs
@@ -1,13 +1,16 @@
 ﻿module Program
 
 open week01
+open Week02
 
 [<EntryPoint>]
 let main argv =
     printfn "\n  Hello from F#"
-    printfn "  Calling Week 01 Exercise 7"
+    printfn "  Calling Week 02 Exercise 1 - 4"
     
-
-    let result = week01.exercise7.run()
+    Week02.exercise01.run()
+    Week02.exercise02.run()
+    Week02.exercise03.run()
+    Week02.exercise04.run()
     0
 
---
+++ Week02.fs
@@ -0,0 +1,28 @@
+module Week02
+
+open System
+
+module exercise01 = 
+    let run () = 
+        let TimesTwo x = x * 2
+        Console.Write("\n  Two Times is: ")
+        Console.WriteLine(TimesTwo 5)
+
+module exercise02 = 
+    let run () = 
+        let Remainder a b = a % b
+        Console.Write("  Remainder is: ")        
+        Console.WriteLine(Remainder 8 5)
+
+module exercise03 = 
+    let run () = 
+        let CubeVolume h w d = h * w * d
+        Console.Write($"  The volume of a cube is: ")
+        Console.WriteLine(CubeVolume 3 4 5)
+
+module exercise04 = 
+    let run () = 
+        let LargerOfTwo a b = if a > b then a else b
+        Console.Write($"  The larger is: ")
+        Console.WriteLine(LargerOfTwo 5 7)
+        Console.WriteLine()
