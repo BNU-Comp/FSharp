23454940f962ca63f53c70a626b43076e8d01716
Derek Peacock <derek.peacock@yahoo.com>
2024-03-06 19:07:43 +0000

    Added mean score

--- Collections.fs
+++ Collections.fs
@@ -4,8 +4,18 @@ module Collections
 open System
 
 
-let printMeanScore row =
-    printfn "  %s" row
+let printMeanScore (row : string) = 
+    let elements = row.Split('\t')
+    let name = elements.[0]
+    let id =  elements.[1]
+    
+    let score = 
+        elements
+        |> Array.skip 2
+        |> Array.map float
+        |> Array.average
+    
+    printfn "  %s\t%s\t%0.1f" name id score
 let readData filePath = 
     let rows = System.IO.File.ReadAllLines(filePath)
     // let data = 
@@ -13,13 +23,14 @@ let readData filePath =
     //     |> Array.map (fun row -> row.Split('\t'))
     //     |> Array.map (fun cols -> (cols.[0], cols.[1], int cols.[2]))
     
-    printfn "  Read %d rows from file\n" rows.Length
+    // printfn "  Read %d rows from file\n" rows.Length
+    
     rows
         |> Array.skip 1 
         |> Array.iter printMeanScore
     
     printfn "  \nFile Read Complete\n"
-    
+
 
 
 module ReadFile = 
